fc2a194ef7a32195279037e91f9b01722b23b5cf
marcloosley <marcloosley@gmail.com>
2014-11-03 15:50:32 +0000

    added the first bit of prac 4 called hansard.py

--- hansard.py
+++ hansard.py
@@ -3,8 +3,14 @@
 def removePunctuation(text):
 	newtext = ""
 	newtext = text.translate(None, '?!:,."-()') #str.translate(table[, deletechars])
-	print newtext
+	print 
 
-
+def readHansard(d, filename):
+	#open the file
+	readfile = open('PMQs_221014.txt')
 	
-
+	#each line should strip off any newline characters, remove punctuation then split the line on spaces into a list of words.
+	#use for loop to conert each word to lower case, add each word as a key into the dictionary.
+	#if the word is already present increment the value by 1.
+	#close the file
+	#purpose is to modify the dict, check the 

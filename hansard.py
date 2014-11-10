b9b803e1a927f40dea0feb25e8178b1153160b0c
marcloosley <marcloosley@gmail.com>
2014-11-10 13:51:54 +0000

    updates to hansard.py, is now working and finished.

--- hansard.py
+++ hansard.py
@@ -1,24 +1,88 @@
+import sys
 
+# Function to look up the counts for a list of words
+# Does not return anything, just prints out the counts
+def compareCounts(d, words):
+  for word in words:
+    if word in d:
+      print(word + " " + str(d[word]))
+    else:
+      print(word + " 0")
 
+	  
+# removePunctuation takes a string parameter and returns
+# a copy of the string with the punctuation removed	  
 def removePunctuation(text):
 	newtext = ""
-	newtext = text.translate(None, '?!:,."-()') #str.translate(table[, deletechars])
-	print 
+	newtext = text.translate(None,'?!:,."-()\n') #str.translate(table[, deletechars])
+	return newtext 
 
+# Writes out the word frequencies to a file.
+def writefrequentWords(d, filename):
+  wordcounts = []
+  for w in d.keys():
+    wordcounts.append ((d[w],w))
+
+  wordcounts.sort(reverse=True)
+
+  outfile = open(filename,"w")
+  for (c,w) in wordcounts:
+    outfile.write(str(c) +  " " +w + "\n")
+    outfile.close
+
+	
+	
+	
 def readHansard(d, filename):
 	#open the file
 	readfile = open(filename)
+	#readfile =  removePunctuation(readfile)
+	
 	for line in readfile:
-		print line		
-	#each line should strip off any newline characters, remove punctuation then split the line on spaces into a list of words.
-	#use for loop to conert each word to lower case, add each word as a key into the dictionary.
-	#if the word is already present increment the value by 1.
-	#close the file
-	#purpose is to modify the dict, check the 
+		line = line.strip()
+		if line != "":
+			nopunc = removePunctuation(line)
+			words = nopunc.split()
+		for word in words:
+			lword = word.lower()
+			if lword in d:
+				d[lword] += 1
+			else:
+				d[lword] = 1
+	readfile.close()
 
 
 
-dict = {}
-readHansard(dict, 'PMQs_221014.txt')
+def test():          
+  # Make a new dictionary
+  wordDict = {}
+  # Read in the text
+  readHansard(wordDict, "PMQs_221014.txt")
+
+  # Lets compare some words.
+
+  print("Are they referring to gentlemen or ladies?")
+  compareCounts(wordDict,["gentleman","lady"])
+  
+  print("Who asks the questions?")
+  compareCounts(wordDict,["con","lab","ld","ukip"])
+  
+  print("What countries are they talking about?")
+  compareCounts(wordDict,["scotland","england","wales"])
+  
+  print("Who are they talking about?")
+  compareCounts(wordDict,["i","we","you","they"])
+
+  # Write the word frequencies to file
+  writefrequentWords(wordDict, "hansout.txt")
+
+
+
+if __name__ == "__main__":
+  test()
+
+
+
+
 
 
